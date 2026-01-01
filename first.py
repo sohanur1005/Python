62cf7d31a4c66f8f139050e5ad6d00db73333dab
sohanur1005 <sohanur.sr1005@gmail.com>
2026-01-01 13:41:21 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,8 @@
+1/2
+print(
+"Hello, World!"
+)
+
+cat=1/2
+print(cat)
+print(2/2)
---
+++ input.py
@@ -0,0 +1,15 @@
+name=input("What's Your name?")
+print("hellow",name)
+
+
+
+
+
+#coment
+'''
+multiple line coment
+djkfhsdjks
+jbdjsdsed
+
+
+'''
