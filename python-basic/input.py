a7aae1a776a523677ff548ca668e5d3b04da64fd
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-06 14:12:56 +0600

    send user input program

--- python-basic/input.py
+++ python-basic/input.py
@@ -1,2 +1,18 @@
 money=input('Give me some money : ')
-print('Here is your money'money)
+print('Here is your money',money)
+
+first_money=input('Kodom ali : ')
+second_money=input('Modon Ali : ')
+
+print('Kodom Ali: ',first_money,'and ','Modon Ali : ',second_money)
+# by default the input form the user will bw string type
+
+
+total=first_money + second_money
+print('Total Taka: ',total)
+
+# convert into integer
+first_money_int=int(first_money)
+second_money_int=int(second_money)
+total_money=first_money_int+second_money_int
+print('Now actual money'total_money)
