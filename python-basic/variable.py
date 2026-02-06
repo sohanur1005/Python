9f9e66a6436f330c452d8eca76eeb1d6eae14d8c
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-06 11:54:47 +0600

    send this portion

---
+++ python-basic/variable.py
@@ -0,0 +1,12 @@
+age=25
+interest_rate= 4
+name="Sohanur Rahman"
+district='Dhaka'
+
+print(age+interest_rate)
+print(type(age))
+print(type(district))
+print(type(name))
+
+text=f'sohanur Rahman {age} live in {district} '
+print(text)
--- python-basic/variavble.py
+++
@@ -1,4 +0,0 @@
-age=25
-interest_rate= 4
-name="Sohanur Rahman"
-district='Dhaka'
