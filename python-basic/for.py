87b114d3c75f293390bf04aa93314eeb38b24795
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-07 11:44:09 +0600

    send this

---
+++ python-basic/for.py
@@ -0,0 +1,11 @@
+numbers=[5,10,15,20,25]
+sum=0
+for num in numbers:
+    print(num)
+    sum=sum+num
+print('Total: ',sum)
+
+# print character
+char='sohanur Rahman'
+for ch in char:
+    print(ch)
---
+++ python-basic/while.py
@@ -0,0 +1,14 @@
+# num=1
+
+# while num<=10:
+#     print(num)
+#     num=num+1
+#     if num==5:
+#         break
+
+n=0
+while n<=10:
+    n=n+1
+    if n%2==1:
+        continue
+    print(n)
