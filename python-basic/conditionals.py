6acd3a61c2feb22bef0000d9c65eb5e72f363ef0
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-07 10:57:44 +0600

    send this

---
+++ python-basic/conditionals.py
@@ -0,0 +1,8 @@
+a=5
+
+if a>3:
+    print('3 er beshi')
+elif a<2:        #else if=elif
+    print('2 er kom')
+else:
+    print('3 er kom')
--- python-basic/operator.py
+++ python-basic/operator.py
@@ -3,4 +3,9 @@ n2=3
 print(n1+n2)
 print(15/4)
 print(15//4) #purno vagfol
-print(15/4) #reminder
+print(15%4) #reminder
+
+power=n1*n2 #multiply
+power2=n1**n2 # power
+print(power)
+print(power2)
